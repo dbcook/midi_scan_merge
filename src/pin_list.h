f9e7c506abb7328f65abbb8bec59826077af983b
Dave Cook <caveduck17@gmail.com>
2026-05-25 23:13:18 -0700

    pin_list.h - remove obsolete struct

--- src/pin_list.h
+++ src/pin_list.h
@@ -7,8 +7,6 @@
 #include "debouncer.h"
 #include "ArrayList.h"
 
-
-
 // Pin block definition for both matrix and parallel scan organization
 // All instances of this should be static (const).  Cannot be a class because of this.
 // Relies on implicitly incrementing MIDI note numbers and debouncer index.
@@ -18,28 +16,8 @@
 // I rejected the idea of making the MIDI output interface configurable per block; instead making a design decision to
 // always send outputs to the same interface.  This means no splitter / distribution capability, but that seems immaterial
 // for the expected use cases of this code.
-//
-// TODO: Implmement hardcoded discontiguous diode matrix pin blocks covering pins 2-3, 5-9, 11-12, 16-53 (cannot use 4, 10, 13, 14-15)
-// TODO: Implement double-contact pin blocks.
-//
-#if 0
-typedef struct PinBlock {
-    bool useSelect;                 // true if block is for a diode matrix using select pins
-    bool activeLow;                 // true if input signals are active low
-    int selectBasePin;              // start of select pin range, ignored if useSelect == false
-    int numSelectPins;              // num of select pins, ignored if useSelect == false
-    int readBasePin;                // start of read pin range
-    int numReadPins;                // num of read pins
-    int numCtrls;                   // total number of controls (e.g. notes) scanned by the block.  Typ. 61 for organ keyboards.
-    midi::DataByte baseMidiNoteNum; // start of contiguous MIDI note num range
-    midi::Channel midiOutChan;      // MIDI output channel for the block
-    uint8_t attackDebounceMsec;     // debounce time for attack
-    uint8_t releaseDebounceMsec;    // debounce time for release
-} PinBlock_t;
-#endif
 
-// new struct with a union for multi-contact blocks
-// this will replace PinBlock_t soon
+// struct with an embedded union for multi-contact blocks
 
 #define MAX_CONTACTS 3
 
@@ -62,13 +40,14 @@ typedef struct PinBlockMultContact {
     PbPinInfo_t pbPinInfo[MAX_CONTACTS];
 } PinBlockMulti_t;
 
-// struct for analog input blocks
+// struct for analog input blocks with consecutive CC numbers and individually tunable low-pass filtering.
 // Resolution: we always tell the Arduino runtimes to operate as if we had 16-bit resolution.
 // There is no harm in that even though older boards have 10 or 12 bit resolution; the LSBs in the 16-bit result are just zero.
 // Filtering is done via the one-line classic exponential smoothing filter
 //   val = (alpha * sample) + (1 - alpha) * val
 // where alpha [0,1) is the sensitivity constant
 // There is no center position parameter since auto centering joysticks are rare within the instrument use case
+// Everything is done in floating point since the Grand Central has an FPU
 typedef struct PinBlockAnalogRead {
     uint8_t basePin;                // starting pin number for this block (easiest to use A0, A1, etc.)
     midi::DataByte baseCCNum;       // starting CC number for inputs in this block
@@ -83,10 +62,7 @@ typedef struct PinBlockAnalogRead {
 // Include specific configuration
 #include "config_scan_pins.h"
 
-// We can take compile time sizes of the hardcoded test flash pinblock groups
-#if 0
-const int nFlashPinBlocks = sizeof(gFlashPinBlocks) / sizeof(PinBlock_t);
-#endif
+// We can take compile time sizes of the hardcoded flash pinblock groups
 const int nFlashPinBlocksMulti = sizeof(gFlashPinBlocksMulti) / sizeof(PinBlockMulti_t);
 const int nFlashPinBlocksAnalogRead = sizeof(gFlashPinBlocksAnalogRead) / sizeof(PinBlockAnalogRead_t);
 
@@ -95,7 +71,6 @@ EXTERN ArrayList<PinBlockMulti_t> gPinBlocksDigital;
 EXTERN ArrayList<PinBlockAnalogRead_t> gPinBlocksAnalog;
 
 void initMemPinBlocks();
-
 int calcPinBlockSize(int pbIndx);
 int calcDebouncerBase(int pbIndx);
 void initDebouncerBases();
