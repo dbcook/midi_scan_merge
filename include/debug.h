0d525551a6b643ed332853392fa6d139ef1f7596
Dave Cook <caveduck17@gmail.com>
2026-05-29 05:05:07 -0700

    debug.h: improve logic and doc for board specific console output device

--- include/debug.h
+++ include/debug.h
@@ -2,11 +2,13 @@
 #include <Arduino.h>
 #include <AppleMIDI.h>
 #include "glob_gen.h"
+#include "serial_gcm4.h"
 
 // Debug console system
-// MUST be included after AppleMidi.h since it nukes AM_DBG symbols unless we let it totally take over the console port.
+// MUST be included AFTER AppleMidi.h since that file nukes AM_DBG symbols unless we let it totally take over the console port.
+//
 // All console code vanishes when USE_DEBUG_PRINT is false
-// If USE_DEBUG_PRINT is true, the MIDI shield (if present) must not use Serial but must use Serial1 - Serial3
+// If USE_DEBUG_PRINT is true, the MIDI shield (if present) must avoid the hardware UART used for the console.
 // ALWAYS use F("msg") to store the literals to flash instead of RAM
 
 #define USE_DEBUG_PRINT true
@@ -15,21 +17,52 @@
 
 const unsigned long consoleBaudRate = 115200;   // must match monitor_speed in platformio.h
 
-// On Grand Central we need class Serial_ (from USBAPI.h) but on Due we need HardwareSerial (hardwareserial.h)
-// Platform IO processor symbols
-//     Grand Central: ARDUINO_ADAFRUIT_GRAND_CENTRAL_M4 (we have to define this in build_flags in platformio.ini)
+// Platform Specifics
+//
+// Assigning the console output port is platform dependent, and rather messy due to port naming irregularity.
+//
+// AdaFruit Grand Central M4:
+// To send console messages back out the USB port on Grand Central we need to use the USB port object
+//    Serial_ Serial
+// while the four hardware UARTs are
+//    Uart Serial1, Serial2, Serial3, Serial4
+// To use Serial2 through Serial4 you have to use the serial_gcm4.h / .cpp I wrote to extend what variant.h / .cpp provide
+//
+// Arduino Due:
+//    On Due the hardware UARTs are all
+//      HardwareSerial Serial, Serial1, Serial2, Serial3
+//    If you put the console on "Serial", the messages go back out the programming port USB but I believe this is tied to GPIO pins 0-1.
+//
+// PlatformIO board identification symbols
+//     Grand Central: ARDUINO_GRAND_CENTRAL_M4 ( see https://github.com/platformio/platform-atmelsam/blob/master/boards/adafruit_grandcentral_m4.json )
 //     Arduino Due  : ARDUINO_SAM_DUE
-#if defined(ARDUINO_SAM_DUE)
+
+#if defined( ARDUINO_SAM_DUE )
+
 EXTERN HardwareSerial *Console
-#elif defined(ARDUINO_ADAFRUIT_GRAND_CENTRAL_M4)
-EXTERN Serial_ *Console
-#else
-#error Unsupported board type!
-#endif
 #ifdef GEN_GLOBALS
 = &Serial
 #endif
 ;
+
+#elif defined( ARDUINO_GRAND_CENTRAL_M4 )
+
+// console on native USB port "Serial_ Serial"
+EXTERN Serial_ *Console
+// alternative console on "Uart Serial1" (pins 0,1), needed when using native USB MIDI transport output
+// To use this you have to hook up a separate USB-serial adapter to the GPIO pins for the Sercom.
+// The TX/RX pins are the same as other Arduino large format boards but they are named Serial1 - Serial4
+// instead of Serial, Serial1, Serial2, Serial3.
+// EXTERN Uart *Console = &Serial1
+#ifdef GEN_GLOBALS
+= &Serial
+#endif
+;
+
+#else
+#error Unsupported board type!
+#endif
+
 
 #define Console_print(...) Console->print(__VA_ARGS__)
 #define Console_println(...) Console->println(__VA_ARGS__); Console->flush()
@@ -37,7 +70,7 @@ EXTERN Serial_ *Console
 
 // modification of the AppleMidi AM_DBG templates from AppleMIDI_Debug.h
 // Make sure AppleMIDI_Debug.h is NOT included by not defining SerialMon
-// and kill macros that it defines if undefined(SerialMon)
+// and kill macros that it defines when undefined(SerialMon)
 #undef SerialMon
 
 #undef AM_DBG_SETUP       // no implementation, now controlled by USE_DEBUG_PRINT
