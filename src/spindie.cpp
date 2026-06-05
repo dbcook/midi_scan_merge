dcf34e2a7e9d168fd1aa41226adea993f5ef9159
Dave Cook <caveduck17@gmail.com>
2026-06-05 03:15:26 -0500

    spindie: drop into infinite yield() rather than hardspin that locks out background IO tasks.

--- src/spindie.cpp
+++ src/spindie.cpp
@@ -3,6 +3,16 @@
 #include "data.h"
 #include "spindie.h"
 
+// The use case here is to display a message about some unrecoverable condition
+// and then stop all foreground processing that might prevent the message from
+// being seen (i.e. an LCD update), while leaving any background IO active.
+//
+// Die by spitting out the message on console and/or LCD and then dropping
+// into a hard loop calling yield().  That allows certain background things to
+// continue (hardware specific).  On AVR type systems the default yield is a
+// weak system function that does nothing but can be overridden as desired.
+// On ESP32 systems with WiFi, the WiFi comms will stop if yield() is not called.
+
 void _SpinDie( const char * msg, int val ) {
     AM_DBG(F("CRASH"), msg, val);
     if (gConfig.useLcd) {
@@ -14,7 +24,8 @@ void _SpinDie( const char * msg, int val ) {
         gLcd->lcdMessage(msg);
         gLcd->lcdMessage(buf);
     }
-    while(1);   // the end
+    while(1)
+        yield();   // the end
 }
 
 // note - snprintf_P format string doesn't interpret %S as documented, it's looking for wchar_t *
@@ -29,7 +40,8 @@ void _SpinDie( const __FlashStringHelper * msg, int val) {
         gLcd->lcdMessage(msg);
         gLcd->lcdMessage(buf);
     }
-    while(1);   // the end
+    while(1)
+        yield();   // the end
 }
 
 void _SpinDie( const __FlashStringHelper * msg ) {
@@ -40,6 +52,8 @@ void _SpinDie( const __FlashStringHelper * msg ) {
         gLcd->pLCD->setCursor(0, 1);
         gLcd->lcdMessage(msg);
     }    
+    while(1)
+        yield();   // the end
 }
 
 void _SpinDie( const char * msg ) {
@@ -50,4 +64,6 @@ void _SpinDie( const char * msg ) {
         gLcd->pLCD->setCursor(0, 1);
         gLcd->lcdMessage(msg);
     }    
+    while(1)
+        yield();   // the end
 }
